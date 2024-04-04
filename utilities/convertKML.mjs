e371f25b23c4e12c27d728826490ee088863d3c6
Courtney Nguyen <hello@courtyen.io>
2024-04-04 14:19:51 -0500

    mint

--- utilities/convertKML.mjs
+++ utilities/convertKML.mjs
@@ -4,7 +4,7 @@ import * as url from "url"
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
 
 fs.readFile(
-  `${__dirname}/Texas_eclipse_v2.8.kml`,
+  `${__dirname}/Texas_eclipse_v2.9.kml`,
   "utf8",
   (err, xmlDataStr) => {
     if (err) {
@@ -25,7 +25,7 @@ fs.readFile(
         Placemark: jsonObj.kml.Document.Placemark,
       }
       fs.writeFile(
-        `${__dirname}/Texas_eclipse_v2.8.json`,
+        `${__dirname}/Texas_eclipse_v2.9.json`,
         JSON.stringify(myMap, null, 2),
         (err) => {
           if (err) {
